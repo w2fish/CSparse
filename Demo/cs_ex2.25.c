839624d9bb68de5f97b242ab0abb73cdef530809
w2fish <nkwxy@aliyun.com>
2015-04-08 17:21:43 +0800

    add cs_subm3 cs_ex2.25

--- Demo/cs_ex2.25.c
+++ Demo/cs_ex2.25.c
@@ -1,19 +1,20 @@
-/* 20150407 */
-/* test cs_subm2 */
-/* cs version of matlab statement C = A(i, j) */
+/* 20150408 */
+/* test cs_subm3 */
+/* cs version of matlab statement A(i, j) = C */
 /* where i, j are integer vectors */
-/* i, j may OR may not contain duplicate indices */
-/* command: cs_ex2.24 printOrNot ilength jlength */
+/* i, j may or may not contain duplicate indices */
+/* command: cs_ex2.25 printOrNot ilength jlength */
 #include "cs.h"
 int main(int argc, char * argv[])
 {
 	int print = atoi(argv[1]) ;
 	int il = atoi(argv[2]) ;
 	int jl = atoi(argv[3]) ;
-	cs *T = NULL, *A = NULL, *C = NULL ;
+	cs *T = NULL, *A = NULL, *C = NULL, *D = NULL ;
 	csi i ;
 	FILE *fp ;
 	char fileMatrix[256] = "fileMatrix" ;
+	char fileMatrix1[256] = "fileMatrix1" ;
 	char fileVectorX[256] = "fileVectorX" ;
 	char fileVectorY[256] = "fileVectorY" ;
 	/* allocate x, y */
@@ -46,36 +47,60 @@ int main(int argc, char * argv[])
 	}
 	A = cs_compress(T) ;
 	T = cs_spfree(T) ;
-	/* print x */
-	printf("x = \n") ;
-	for(i=0; i<il; i++)
+	/* load C */
+	fp = fopen(fileMatrix1, "r") ;
+	if (!fp)
 	{
-		printf("%d\n", x[i]) ;
+		printf("open fileMatrix1 fail, quit\n") ; return 0 ;
 	}
-	/* print y */
-	printf("y = \n") ;
-	for(i=0; i<jl; i++)
+	T = cs_load(fp) ;
+	printf("T = \n") ; cs_print(T, 1) ;
+	fclose(fp) ;
+	if (!T)
 	{
-		printf("%d\n", y[i]) ;
+		printf("load T fail, quit\n") ; return 0 ;
+	}
+	C = cs_compress(T) ;
+	T = cs_spfree(T) ;
+	
+	/* print */
+	if (print)
+	{
+		/* print x */
+		printf("x = \n") ;
+		for(i=0; i<il; i++)
+		{
+			printf("%d\n", x[i]) ;
+		}
+		/* print y */
+		printf("y = \n") ;
+		for(i=0; i<jl; i++)
+		{
+			printf("%d\n", y[i]) ;
+		}
+		printf("A = \n") ; cs_print(A, 0) ;
+		printf("C = \n") ; cs_print(C, 0) ;
 	}
 	/* calculate C */
-	C = cs_subm2(A, x, il, y, jl) ;
-	if (!C) 
+	D = cs_subm3(A, C, x, il, y, jl) ;
+	if (!D) 
 	{
-		printf("cs_subm2 fail, quit\n") ; return 0 ;
+		printf("cs_subm3 fail, quit\n") ; return 0 ;
 	}
 	/* print */
 	if (print)
 	{
-		printf("A = \n") ; cs_print(A, 0) ;
-		printf("C = \n") ; cs_print(C, 0) ;
+		printf("D = \n") ; cs_print(D, 0) ;
 	}
 	/* release */
 	printf("release...\n") ;
 	A = cs_spfree(A) ;
+	C = cs_spfree(C) ;
+	D = cs_spfree(D) ;
 	free(x) ;
 	free(y) ;
 	printf("release done.\n") ;
 	
 	return 0 ;
 }
+
--- Source/cs_subm3.c
+++ Source/cs_subm3.c
@@ -1,9 +1,8 @@
-/* 20150407 */
+/* 20150408 */
 /* Tim.D. book, ex2.25 */
 /* cs version of matlab statement A(i, j) = C */
 /* where i, j are integer vectors */
-/* Currently, i, j does NOT contain duplicate indices */
-/* because I can NOT figure out how i, j has duplicate indices */
+/* i, j may or may not contain duplicate indices */
 #include "cs.h"
 cs *cs_subm3(cs *A, cs *C, int *iv, int il, int *jv, int jl)
 {
@@ -16,37 +15,94 @@ cs *cs_subm3(cs *A, cs *C, int *iv, int il, int *jv, int jl)
 		printf("input size wrong, quit\n") ; return NULL ;
 	}
 	cs *D = NULL ;  /* the output matrix */
-	csi m, n, i, j, p, nz = 0 ;
-	csi *Ai, *Ap, *Ci, *Cp, *Di, *Dp ;
-	double *Ax, *Cx, *Dx ;
+	csi m, n, i, j, p, nz = 0, t, s, r ;
+	csi *Ai, *Ap, *Ci, *Cp, *Di, *Dp, *w, *iw, *jw ;
+	double *Ax, *Cx, *Dx, *x ;
 	int a, b ;
+	Ai = A->i ; Ap = A->p ; Ax = A->x ;
+	Ci = C->i ; Cp = C->p ; Cx = C->x ;
 	/* find m and n for D */
+	m = A->m ;
 	for (a=0; a<il; a++)
 	{
 		b = iv[a] ; 
-		m = CS_MAX(A->m, b) ;
+		m = CS_MAX(m, b+1) ;
 	}
+	n = A->n ;
 	for (a=0; a<jl; a++)
 	{
 		b = jv[a] ; 
-		n = CS_MAX(A->n, b) ;
+		n = CS_MAX(n, b+1) ;
 	}
 	/* allocate D */
 	D = cs_spalloc(m, n, m + n, 1, 0) ;
+	w = cs_calloc(m, sizeof(csi)) ;
+	iw = cs_calloc(C->m, sizeof(csi)) ;
+	jw = cs_calloc(n, sizeof(csi)) ;
+	x = cs_malloc(m, sizeof(double)) ;
 	Dp = D->p ;
+	/* initialize workspace */
+	for (i=0; i<C->m; i++) iw[i] = -1 ;
+	for (j=0; j<n; j++) jw[j] = -1 ;
+	/* set iw, match row index of C to D */
+	for (a=0; a<il; a++)
+	{
+		iw[a] = iv[a] ;
+	}
+	/* set jw, match col index of D to C */
+	p = 0 ;
+	for (a=0; a<jl; a++)
+	{
+		jw[jv[a]] = p++ ;
+	}
 	/* copy A and C to D */
 	for (j=0; j<n; j++)
 	{
-		if (nz + m > D>nzmax && !cs_sprealloc(D, 2*D->nzmax+m))
+		if (nz + m > D->nzmax && !cs_sprealloc(D, 2*D->nzmax+m))
 		{
 			printf("reallocate D fail, quit\n") ;
+			iw = cs_free(iw) ;
+			jw = cs_free(jw) ;
+			x = cs_free(x) ;
 			return cs_done(D, w, NULL, 0) ;
 		}
 		Di = D->i ; Dx = D->x ;
 		Dp[j] = nz ;
-		for (
-
-
-
+		/* copy j-th col of A to D */
+		if (j < A->n)
+		{
+			for (p=Ap[j]; p<Ap[j+1]; p++)
+			{
+				i = Ai[p] ;
+				w[i] = j + 1 ;
+				Di[nz++] = i ;
+				x[i] = Ax[p] ;
+			}
+		}
+		/* copy t-th col of C to D */
+		if (jw[j] != -1)
+		{
+			t = jw[j] ;  /* col index in C */
+			for(s=Cp[t]; s<Cp[t+1]; s++)
+			{
+				r = iw[Ci[s]] ;  /* row index in D */
+				if (w[r] < j + 1)
+				{
+					w[r] = j + 1 ;
+					Di[nz++] = r ;
+				}
+				x[r] = Cx[s] ;
+			}
+	 	}
+		for (p=Dp[j]; p<nz ; p++) Dx[p] = x[Di[p]] ;
+	}
+	Dp[n] = nz ;
+	cs_sprealloc(C, 0) ;
+	/* release */
+	w = cs_free(w) ;
+	iw = cs_free(iw) ;
+	jw = cs_free(jw) ;
+	x = cs_free(x) ;
 	
-
+	return D ;
+}
